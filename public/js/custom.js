13e57363b6d2513c14d83337aba5899bac6dfe6b
Soju Desmond <>
2020-06-24 01:48:14 +0700

    update public

---
+++ public/js/custom.js
@@ -0,0 +1,10 @@
+
+//Lunar Modal
+$('.modal').on('show.bs.modal', function (e) {
+    if($(e.currentTarget).attr("data-popup")){
+        $("body").addClass("body-scrollable");
+    }
+});
+$('.modal').on('hidden.bs.modal', function (e) {
+    $("body").removeClass("body-scrollable");
+});
